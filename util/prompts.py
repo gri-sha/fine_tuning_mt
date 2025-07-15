2b72fd43733b2c595def41e3ec0b14307c57365f
gri-sha <183693051+gri-sha@users.noreply.github.com>
2025-07-15 17:14:19 +0200

    restructure 2.5

--- util/prompts.py
+++ util/prompts.py
@@ -54,16 +54,24 @@ def generate_simple_eval_prompts(
         prompts.append(prompt)
     return df["en"].to_list(), df["fr"].to_list(), prompts
 
+def generate_prompts_as_text_and_labels(df: DataFrame, shots: int = 0, fuzzy: bool = True) -> list[str, str]:
+    if shots < 0:
+        raise ValueError('Argument "shots" must be non-negative integer')
 
-# def generate_instructive_prompt(target: str, examples: list[tuple[str]] = []) -> str:
-#     # Pretrained, but not fine tuned model
-#     # Tags [INST], <s> are used only for fine tuned Mistral 7B (e.g. Mistral-7B-Instruct)
-#     if examples:
-#         header = "You are a helpful translation assistant. Your task is to translate the given English sentence to French based on the following examples:\n"
-#         shots = ""
-#         for elem in examples:
-#             shots += f"English: {elem[0]} -> French: {elem[1]}\n"
-#         footer = f"Output only the translated sentence without explanations.\nTranslate the following sentence to French:\n{target}"
-#         return header + shots + footer
-#     else:
-#         return f"You are a helpful translation assistant. Your task is to translate the given English sentence to French.\nOutput only the translated sentence without explanations.\nTranslate the following sentence to French:\n{target}"
+    prompts = []
+    labels = []
+
+    for i in range(len(df)):
+        prompt = ""
+        if shots > 0:
+            if fuzzy:
+                for j in range(shots):
+                    prompt += f'English: {df["match"][i][j][0]}\nFrench: {df["match"][i][j][1]}\n'
+            else:
+                random_indexes = random.sample(range(len(df)), shots)
+                for idx in random_indexes:
+                    prompt += f'English: {df["en"][idx]}\nFrench: {df["fr"][idx]}\n'
+        prompt += f'English: {df["en"][i]}\nFrench: '
+        prompts.append(prompt)
+        labels.append(prompt + df["fr"][i])
+    return prompts, labels
